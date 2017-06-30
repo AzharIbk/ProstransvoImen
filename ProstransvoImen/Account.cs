32072212213c9fe52ace6be0505d39b001caf68e
ИбкеноваА <@403_3_>
2017-06-30 21:55:11 +0600

    Add project files.

---
+++ ProstransvoImen/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankomat
+{
+    namespace NAccount
+    {
+        class Account
+        {
+
+            string accNumber;
+            string pass;
+            double amount;
+
+            string CreateAccNumber()
+            {
+                Random rnum = new Random();
+                accNumber = rnum.Next(1, 100).ToString();
+                return accNumber;
+            }
+
+            string CreatePass()
+            {
+                Random rnum = new Random();
+                pass="pass"+rnum
+            }         
+
+        }
+    }
+}
